d05b14ceca19da3aa5f8abb6daa91c0d7b77e1fc
Tanya <Tanya@TanyadeMacBook-Air.local>
2021-04-25 17:26:09 +0800

    week2 ver1

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,11 @@
-function printStars(n) {
-
+/* eslint-disable */ 
+//HW1
+//練習三 寫出一個能印出n個*的函式
+function printstar (n){
+    var result = ''
+    for (var i = 1; i<=n; i++){
+         result += '*'
+    }
+    console.log(result)
 }
-
-printStars(5)
+printstar(2)
---
+++ homeworks/week3/hw3.js
@@ -0,0 +1,43 @@
+
+/* eslint-disable */ 
+//判斷是不是質數 檢查n 能不能被1~n當中除了1和n以外的數整除，如果不能就是質數
+var readline = require('readline');
+var rl = readline.createInterface({
+  input: process.stdin
+});
+
+var lines = []
+
+//讀取到一行，先把這一行加進去 lines 陣列，最後再一起處理
+rl.on('line', function (line) {
+  lines.push(line)
+});
+
+//輸入結束，開始針對 lines 做處理
+rl.on('close', function() {
+  solve(lines)
+})
+
+//上面都不用管，只需要完成這個 function 就好，可以透過 lines[i] 拿取內容
+function solve(lines) {
+    for(i=1; i<=(lines[0]); i++){
+    let quanInSeq = Number(lines[0])
+    let num = Number(lines[i])
+    if(primeInspect(num)){
+        console.log("Prime")
+    } else{
+        console.log("Composite")
+    }
+}
+}
+
+function primeInspect (num){
+    if(num ===1) return false
+    if(num ===2) return true
+    for(i=2; i<num; i++){
+        if(num % i === 0) {
+            return false
+        }
+    }  
+    return true
+}
