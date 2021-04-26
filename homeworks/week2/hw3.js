0e96e024605b679c7ec72270a989c3f8bfc4b6f4
Tanya <Tanya@TanyadeMacBook-Air.local>
2021-04-26 18:44:12 +0800

    week2 ver3

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,13 @@
-function reverse(str) {
-  
-}
+/* eslint-disable */ 
+//HW3：反轉字串 給定一個字串，請「印出」反轉之後的樣子（不能使用內建的 reverse 函式）
+//reverse('yoyoyo') 正確輸出：oyoyoy
 
-reverse('hello');
+function reverse(str){
+    var reverseStr = ''
+    for (var i = str.length -1; i>=0; i--){
+        reverseStr += str[i]
+        //console.log(str[i]) //可看每一次迴圈印出的
+    }
+    return reverseStr
+}
+console.log(reverse('hello'));
--- homeworks/week2/hw4.js
+++ homeworks/week2/hw4.js
@@ -1,5 +1,20 @@
-function printFactor(n) {
-  
+/* eslint-disable */ 
+//HW4 印出因數
+//版本一 印出因數
+function findFactor(n) {
+    var divisor = ''
+    var answer = []
+    for (i= 1; i<=n; i++){
+        if (n % i ===0){   //如果這個除數可以整除，代表是因數
+            divisor = i
+            answer.push(divisor)
+            //console.log(divisor) //把 divisor 的值放進 result 陣列中
+        } else {
+        }  
+    }  
+    for (i=0; i<answer.length; i++){
+        console.log(answer[i])
+    }
 }
 
-printFactor(10);
+findFactor(10);
